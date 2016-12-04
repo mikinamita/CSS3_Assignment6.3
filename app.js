6899950c862a1a44bbf638987ccdb0133981d5f3
mikinamita <mikinamita@gmail.com>
2016-12-04 10:46:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var i;
+var sum=0;
+for(i=1;i<=1000;i++)
+{
+	sum=sum+i;
+}
+console.log("Sum using for loop :"+sum);
+while(i<=1000)
+{
+	sum=sum+i;
+	i++;
+}
+
+console.log("Sum using while loop :"+sum);
